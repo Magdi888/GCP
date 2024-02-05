4b1c8b946c20abc3d32a45409ef89cf8d26659e8
Ahmed Magdy Baker Abdelrahman <amagdy@ntgclarity.com>
2024-02-05 18:19:49 +0200

    add foundations

---
+++ 00_Fountations/providers.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "5.14.0"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.25.2"
+    }
+  }
+  backend "gcs" {
+    bucket  = "backendstate"
+    prefix  = "terraform/00_foundation"
+  }
+}
+
+
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+data "google_client_config" "default" {
+  depends_on = [module.gke-cluster]
+}
+
+provider "kubernetes" {
+  host                   = module.gke.endpoint
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(module.gke.ca_certificate)
+}
+  
