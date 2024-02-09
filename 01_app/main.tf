844acda22b7c89dedc143e4d99197146259440b3
Ahmed Magdi <a.magdi888@gmail.com>
2024-02-09 19:19:55 +0200

    cert-manager

--- 01_app/main.tf
+++ 01_app/main.tf
@@ -21,13 +21,12 @@ resource "helm_release" "eso" {
   timeout          = 300
   atomic           = true
   create_namespace = true
-  verify = false
-  depends_on = [ module.my-app-workload-identity ]
+  verify           = false
 }
 
-module "my-app-workload-identity" {
+module "external-secrets-workload-identity" {
   source              = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
-  name                = "${var.project_id}.svc.id.goog[external-secrets/external-secrets]"
+  name                = "external-secrets"
   namespace           = "external-secrets"
   use_existing_k8s_sa = true
   cluster_name        = "${var.cluster_name}-${var.env_name}"
@@ -35,5 +34,37 @@ module "my-app-workload-identity" {
   project_id          = var.project_id
   roles               = ["roles/secretmanager.secretAccessor"]
   annotate_k8s_sa     = true
+  module_depends_on   = [helm_release.eso]
 }
+
+# resource "helm_release" "certm" {
+#   name             = "cert-manager"
+#   namespace        = "cert-manager"
+#   repository       = "https://charts.jetstack.io"
+#   chart            = "cert-manager"
+#   version          = "1.12.0"
+#   timeout          = 300
+#   atomic           = true
+#   create_namespace = true
+#   verify           = false
+
+#   values = [
+#     <<YAML
+#     installCRDs: true
+#     YAML
+#   ]
+# }
+
+# module "certm-workload-identity" {
+#   source              = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
+#   name                = "cert-manager"
+#   namespace           = "cert-manager"
+#   use_existing_k8s_sa = true
+#   cluster_name        = "${var.cluster_name}-${var.env_name}"
+#   location            = var.zones[0]
+#   project_id          = var.project_id
+#   roles               = ["roles/dns.admin"]
+#   annotate_k8s_sa     = true
+#   module_depends_on   = [helm_release.cetm]
+# }
 
